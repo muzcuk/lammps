30a8ae7ca6a15ff25ac9ea57d7bc81160cdace4e
Murat Ozturk <murozturk@ku.edu.tr>
2014-11-17 18:04:47 +0200

    replace broken header files

--- src/MOLECULE/bond_harcos.h
+++ src/MOLECULE/bond_harcos.h
@@ -11,236 +11,47 @@
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
-#include "math.h"
-#include "stdlib.h"
-#include "bond_harcos.h"
-#include "atom.h"
-#include "neighbor.h"
-#include "domain.h"
-#include "comm.h"
-#include "force.h"
-#include "memory.h"
-#include "error.h"
-#include "math_const.h"
+#ifdef BOND_CLASS
 
-using namespace LAMMPS_NS;
-using namespace MathConst;
+BondStyle(harcos,BondHarcos)
 
-/* ---------------------------------------------------------------------- */
+#else
 
-BondHarcos::BondHarcos(LAMMPS *lmp) : Bond(lmp) {}
+#ifndef LMP_BOND_HARCOS_H
+#define LMP_BOND_HARCOS_H
 
-/* ---------------------------------------------------------------------- */
+#include "stdio.h"
+#include "bond.h"
 
-BondHarcos::~BondHarcos()
-{
-  if (allocated) {
-    memory->destroy(setflag);
-    memory->destroy(k);
-    memory->destroy(r0);
-    memory->destroy(rCut);
-    memory->destroy(epsilon);
-  }
+namespace LAMMPS_NS {
+
+class BondHarcos : public Bond {
+ public:
+  BondHarcos(class LAMMPS *);
+  virtual ~BondHarcos();
+  virtual void compute(int, int);
+  void coeff(int, char **);
+  double equilibrium_distance(int);
+  void write_restart(FILE *);
+  void read_restart(FILE *);
+  void write_data(FILE *);
+  double single(int, double, int, int, double &);
+
+ protected:
+  double *k,*r0,*rCut,*epsilon;
+
+  void allocate();
+};
+
 }
 
-/* ---------------------------------------------------------------------- */
+#endif
+#endif
 
-void BondHarcos::compute(int eflag, int vflag)
-{
-  int i1,i2,n,type;
-  double delx,dely,delz,ebond,fbond;
-  double rsq,r,dr,rk;
-  double mult;
+/* ERROR/WARNING messages:
 
-  ebond = 0.0;
-  if (eflag || vflag) ev_setup(eflag,vflag);
-  else evflag = 0;
+E: Incorrect args for bond coefficients
 
-  double **x = atom->x;
-  double **f = atom->f;
-  int **bondlist = neighbor->bondlist;
-  int nbondlist = neighbor->nbondlist;
-  int nlocal = atom->nlocal;
-  int newton_bond = force->newton_bond;
+Self-explanatory.  Check the input script or data file.
 
-  for (n = 0; n < nbondlist; n++) {
-    i1 = bondlist[n][0];
-    i2 = bondlist[n][1];
-    type = bondlist[n][2];
-
-    delx = x[i1][0] - x[i2][0];
-    dely = x[i1][1] - x[i2][1];
-    delz = x[i1][2] - x[i2][2];
-
-    rsq = delx*delx + dely*dely + delz*delz;
-    r = sqrt(rsq);
-   
-    if (r<r0[type]) {  		//this is the harmonic part
-    	dr = r - r0[type];
-    	rk = k[type] * dr;
-
-    	// force & energy
-
-    	if (r > 0.0) fbond = -2.0*rk/r;
-    	else fbond = 0.0;
-
-    	if (eflag) ebond = rk*dr - epsilon[type];
-//	printf("harmonic");
-    	
-    }
-
-    else if (r<rCut[type]) { 	//this is the cosine part
-	dr = r - r0[type];
-
-	rk = MY_PI / (rCut[type]-r0[type]);
-	
-	if (eflag) ebond = epsilon[type] * 0.5 * ( 1 - cos(dr*rk)) - epsilon[type];
-
-	if (r > 0.0) fbond =  -epsilon[type] * 0.5 * rk * sin(dr*rk)/r;
-	else fbond = 0.0;
-
-//	printf("cosine");
-    }
-
-    else     {
-//	printf("broken");
-	continue; 	 	//this is a broken bond
-    }
-
-    //printf(" %f %f %f\n",delx*fbond, dely*fbond, delz*fbond );
-    
-    // apply force to each of 2 atoms
-
-    if (newton_bond || i1 < nlocal) {
-      f[i1][0] += delx*fbond;
-      f[i1][1] += dely*fbond;
-      f[i1][2] += delz*fbond;
-    }
-
-    if (newton_bond || i2 < nlocal) {
-      f[i2][0] -= delx*fbond;
-      f[i2][1] -= dely*fbond;
-      f[i2][2] -= delz*fbond;
-    }
-
-    // tally energy if necessary
-
-    if (evflag) ev_tally(i1,i2,nlocal,newton_bond,ebond,fbond,delx,dely,delz);
-  }
-}
-
-/* ---------------------------------------------------------------------- */
-
-void BondHarcos::allocate()
-{
-  allocated = 1;
-  int n = atom->nbondtypes;
-
-  memory->create(k,n+1,"bond:k");
-  memory->create(r0,n+1,"bond:r0");
-  memory->create(rCut,n+1,"bond:rCut"); 
-  memory->create(epsilon,n+1,"bond:epsilon"); 
-
-  memory->create(setflag,n+1,"bond:setflag");
-  for (int i = 1; i <= n; i++) setflag[i] = 0;
-}
-
-/* ----------------------------------------------------------------------
-   set coeffs for one or more types
-------------------------------------------------------------------------- */
-
-void BondHarcos::coeff(int narg, char **arg)
-{
-  if (narg != 5) error->all(FLERR,"Incorrect args for bond coefficients");
-  if (!allocated) allocate();
-
-  int ilo,ihi;
-  force->bounds(arg[0],atom->nbondtypes,ilo,ihi);
-
-  double k_one = force->numeric(FLERR,arg[1]);
-  double r0_one = force->numeric(FLERR,arg[2]);
-  double rCut_one = force->numeric(FLERR,arg[3]);
-  
-  if (r0_one >= rCut_one) error->all(FLERR,"rCut must be greater than r0.");
-
-  double epsilon_one = force->numeric(FLERR,arg[4]);
-
-  int count = 0;
-  for (int i = ilo; i <= ihi; i++) {
-    k[i] = k_one;
-    r0[i] = r0_one;
-    rCut[i] = rCut_one;
-    epsilon[i] = epsilon_one;
-    setflag[i] = 1;
-    count++;
-  }
-
-  if (count == 0) error->all(FLERR,"Incorrect args for bond coefficients");
-}
-
-/* ----------------------------------------------------------------------
-   return an equilbrium bond length
-------------------------------------------------------------------------- */
-
-double BondHarcos::equilibrium_distance(int i)
-{
-  return r0[i];
-}
-
-/* ----------------------------------------------------------------------
-   proc 0 writes out coeffs to restart file
-------------------------------------------------------------------------- */
-
-void BondHarcos::write_restart(FILE *fp)
-{
-  fwrite(&k[1],sizeof(double),atom->nbondtypes,fp);
-  fwrite(&r0[1],sizeof(double),atom->nbondtypes,fp);
-  fwrite(&rCut[1],sizeof(double),atom->nbondtypes,fp);
-  fwrite(&epsilon[1],sizeof(double),atom->nbondtypes,fp);
-}
-
-/* ----------------------------------------------------------------------
-   proc 0 reads coeffs from restart file, bcasts them
-------------------------------------------------------------------------- */
-
-void BondHarcos::read_restart(FILE *fp)
-{
-  allocate();
-
-  if (comm->me == 0) {
-    fread(&k[1],sizeof(double),atom->nbondtypes,fp);
-    fread(&r0[1],sizeof(double),atom->nbondtypes,fp);
-    fread(&rCut[1],sizeof(double),atom->nbondtypes,fp);
-    fread(&epsilon[1],sizeof(double),atom->nbondtypes,fp);
-  }
-  MPI_Bcast(&k[1],atom->nbondtypes,MPI_DOUBLE,0,world);
-  MPI_Bcast(&r0[1],atom->nbondtypes,MPI_DOUBLE,0,world);
-  MPI_Bcast(&rCut[1],atom->nbondtypes,MPI_DOUBLE,0,world);
-  MPI_Bcast(&epsilon[1],atom->nbondtypes,MPI_DOUBLE,0,world);
-
-  for (int i = 1; i <= atom->nbondtypes; i++) setflag[i] = 1;
-}
-
-/* ----------------------------------------------------------------------
-   proc 0 writes to data file
-------------------------------------------------------------------------- */
-
-void BondHarcos::write_data(FILE *fp)
-{
-  for (int i = 1; i <= atom->nbondtypes; i++)
-    fprintf(fp,"%d %g %g %g %g\n",i,k[i],r0[i],rCut[i],epsilon[i]);
-}
-
-/* ---------------------------------------------------------------------- */
-
-double BondHarcos::single(int type, double rsq, int i, int j,
-                        double &fforce)
-{
-  printf ("single() is called for bond_harcos.");
-  double r = sqrt(rsq);
-  double dr = r - r0[type];
-  double rk = k[type] * dr;
-  fforce = 0;
-  if (r > 0.0) fforce = -2.0*rk/r;
-  return rk*dr;
-}
+*/
--- src/MOLECULE/dihedral_breakable.h
+++ src/MOLECULE/dihedral_breakable.h
@@ -11,383 +11,58 @@
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
-/* ----------------------------------------------------------------------
-   Contributing author: Paul Crozier (SNL)
-------------------------------------------------------------------------- */
+#ifdef DIHEDRAL_CLASS
 
-#include "lmptype.h"
-#include "mpi.h"
-#include "math.h"
-#include "stdlib.h"
-#include "dihedral_breakable.h"
-#include "atom.h"
-#include "comm.h"
-#include "neighbor.h"
-#include "domain.h"
-#include "force.h"
-#include "update.h"
-#include "memory.h"
-#include "error.h"
-#include "math_const.h"
+DihedralStyle(breakable,DihedralBreakable)
 
-using namespace LAMMPS_NS;
-using namespace MathConst;
+#else
 
+#ifndef LMP_DIHEDRAL_BREAKABLE_H
+#define LMP_DIHEDRAL_BREAKABLE_H
 
-#define TOLERANCE 0.05
-#define SMALL     0.001
+#include "stdio.h"
+#include "dihedral.h"
 
-/* ---------------------------------------------------------------------- */
+namespace LAMMPS_NS {
 
-DihedralBreakable::DihedralBreakable(LAMMPS *lmp) : Dihedral(lmp)
-{
-  writedata = 1;
+class DihedralBreakable : public Dihedral {
+ public:
+  DihedralBreakable(class LAMMPS *);
+  virtual ~DihedralBreakable();
+  virtual void compute(int, int);
+  void coeff(int, char **);
+  void write_restart(FILE *);
+  void read_restart(FILE *);
+  void write_data(FILE *);
+
+ protected:
+  double *k,*shift,*cos_shift,*sin_shift,*rEq,*rCut,*vShift;
+
+  void allocate();
+};
+
 }
 
-/* ---------------------------------------------------------------------- */
+#endif
+#endif
 
-DihedralBreakable::~DihedralBreakable()
-{
-  if (allocated) {
-    memory->destroy(setflag);
-    memory->destroy(k);
-    memory->destroy(cos_shift);
-    memory->destroy(sin_shift);
-    
-    //new variables for breakable bond
-    memory->destroy(shift);
-    memory->destroy(rEq);
-    memory->destroy(rCut);
-    memory->destroy(vShift);
-  }
-}
+/* ERROR/WARNING messages:
 
-/* ---------------------------------------------------------------------- */
+W: Dihedral problem: %d %ld %d %d %d %d
 
-void DihedralBreakable::compute(int eflag, int vflag)
-{
-  int i1,i2,i3,i4,n,type;
-  double vb1x,vb1y,vb1z,vb2x,vb2y,vb2z,vb3x,vb3y,vb3z,vb2xm,vb2ym,vb2zm;
-  double edihedral,f1[3],f2[3],f3[3],f4[3];
-  double ax,ay,az,bx,by,bz,rasq,rbsq,rg,rginv,ra2inv,rb2inv,rabinv;
-  double df,df1,fg,hg,fga,hgb,gaa,gbb;
-  double dtfx,dtfy,dtfz,dtgx,dtgy,dtgz,dthx,dthy,dthz;
-  double c,s,p,sx2,sy2,sz2;
-  double kMult, dkMult;
+Conformation of the 4 listed dihedral atoms is extreme; you may want
+to check your simulation geometry.
 
-  edihedral = 0.0;
-  if (eflag || vflag) ev_setup(eflag,vflag);
-  else evflag = 0;
+E: Incorrect args for dihedral coefficients
 
-  double **x = atom->x;
-  double **f = atom->f;
-  int **dihedrallist = neighbor->dihedrallist;
-  int ndihedrallist = neighbor->ndihedrallist;
-  int nlocal = atom->nlocal;
-  int newton_bond = force->newton_bond;
+Self-explanatory.  Check the input script or data file.
 
-  for (n = 0; n < ndihedrallist; n++) {
-    i1 = dihedrallist[n][0];
-    i2 = dihedrallist[n][1];
-    i3 = dihedrallist[n][2];
-    i4 = dihedrallist[n][3];
-    type = dihedrallist[n][4];
+E: Incorrect sign arg for dihedral coefficients
 
-    // 1st bond
+Self-explanatory.  Check the input script or data file.
 
-    vb1x = x[i1][0] - x[i2][0];
-    vb1y = x[i1][1] - x[i2][1];
-    vb1z = x[i1][2] - x[i2][2];
+E: Incorrect multiplicity arg for dihedral coefficients
 
-    // 2nd bond
+Self-explanatory.  Check the input script or data file.
 
-    vb2x = x[i3][0] - x[i2][0];
-    vb2y = x[i3][1] - x[i2][1];
-    vb2z = x[i3][2] - x[i2][2];
-
-    vb2xm = -vb2x;
-    vb2ym = -vb2y;
-    vb2zm = -vb2z;
-
-    //calculate length of middle bond (vb2)     
-    rg = sqrt(vb2x*vb2x + vb2y*vb2y + vb2z*vb2z) ;
-    kMult = 1;
-    dkMult = 0;
-    
-    //scale the coefficient if bond is further than equilibrium distance
-    if (rg > rEq[type] && rg < rCut[type]) {
-	rasq = MY_PI / (rCut[type]-rEq[type]); 	//scaling coefficient (use rasq temporarily for efficiency)
-	rbsq = rg - rEq[type]; 			//rdistance from r0   (use rbsq temporarily for efficiency)
-   	kMult  = 0.5 * ( 1 + cos(rbsq*rasq)) ;
-	dkMult = -0.5*rasq * sin(rbsq*rasq)  ;
-	//printf("Scaled dihedral due to bond btw %d and %d with r=%g, kScaled=%g\n" ,i2, i3, rMid,kScaled);
-	//printf("Scaled dihedral r=%.2f, kMult=%.2f, dkMult=%.2f\n" ,rg,kMult,dkMult);
-    }
-
-    //skip this dihedral if bond is broken
-    else if (rg >= rCut[type] )  {
-        //printf("Broken bond btw %d and %d with r=  %g\n" ,i2, i3, rg);
-        //printf("Broken bond with r=%.2f\n" , rg);
-        continue;   
-    }
-
-    // 3rd bond
-
-    vb3x = x[i4][0] - x[i3][0];
-    vb3y = x[i4][1] - x[i3][1];
-    vb3z = x[i4][2] - x[i3][2];
-
-    // c,s calculation
-
-    ax = vb1y*vb2zm - vb1z*vb2ym;
-    ay = vb1z*vb2xm - vb1x*vb2zm;
-    az = vb1x*vb2ym - vb1y*vb2xm;
-    bx = vb3y*vb2zm - vb3z*vb2ym;
-    by = vb3z*vb2xm - vb3x*vb2zm;
-    bz = vb3x*vb2ym - vb3y*vb2xm;
-
-    rasq = ax*ax + ay*ay + az*az;
-    rbsq = bx*bx + by*by + bz*bz;
-
-    rginv = ra2inv = rb2inv = 0.0;
-    if (rg > 0) rginv = 1.0/rg;
-    if (rasq > 0) ra2inv = 1.0/rasq;
-    if (rbsq > 0) rb2inv = 1.0/rbsq;
-    rabinv = sqrt(ra2inv*rb2inv);
-
-    c = (ax*bx + ay*by + az*bz)*rabinv;
-    s = rg*rabinv*(ax*vb3x + ay*vb3y + az*vb3z);
-
-    // error check
-
-    if (c > 1.0 + TOLERANCE || c < (-1.0 - TOLERANCE)) {
-      int me;
-      MPI_Comm_rank(world,&me);
-      if (screen) {
-        char str[128];
-        sprintf(str,"Dihedral problem: %d " BIGINT_FORMAT " " 
-                TAGINT_FORMAT " " TAGINT_FORMAT " " 
-                TAGINT_FORMAT " " TAGINT_FORMAT,
-                me,update->ntimestep,
-                atom->tag[i1],atom->tag[i2],atom->tag[i3],atom->tag[i4]);
-        error->warning(FLERR,str,0);
-        fprintf(screen,"  1st atom: %d %g %g %g\n",
-                me,x[i1][0],x[i1][1],x[i1][2]);
-        fprintf(screen,"  2nd atom: %d %g %g %g\n",
-                me,x[i2][0],x[i2][1],x[i2][2]);
-        fprintf(screen,"  3rd atom: %d %g %g %g\n",
-                me,x[i3][0],x[i3][1],x[i3][2]);
-        fprintf(screen,"  4th atom: %d %g %g %g\n",
-                me,x[i4][0],x[i4][1],x[i4][2]);
-      }
-    }
-
-    if (c > 1.0) c = 1.0;
-    if (c < -1.0) c = -1.0;
-
-    // V(phi) = f(rMid) * p(phi)
-    // f(r)   = kMult (piecewise)
-    // p(phi) = k * [ 1 - cos (phi - shift) ] - vShift 
-
-    p = 1 - c*cos_shift[type] - s*sin_shift[type];
-    df1 =   s*cos_shift[type] - c*sin_shift[type];
-
-    p = k[type]*p - vShift[type];
-
-    if (eflag) edihedral = kMult * p;
-
-    // force calculation begins here
-
-    fg = vb1x*vb2xm + vb1y*vb2ym + vb1z*vb2zm;
-    hg = vb3x*vb2xm + vb3y*vb2ym + vb3z*vb2zm;
-    fga = fg*ra2inv*rginv;
-    hgb = hg*rb2inv*rginv;
-    gaa = -ra2inv*rg;
-    gbb = rb2inv*rg;
-
-    dtfx = gaa*ax;
-    dtfy = gaa*ay;
-    dtfz = gaa*az;
-    dtgx = fga*ax - hgb*bx;
-    dtgy = fga*ay - hgb*by;
-    dtgz = fga*az - hgb*bz;
-    dthx = gbb*bx;
-    dthy = gbb*by;
-    dthz = gbb*bz;
-
-    df = -kMult * k[type] * df1;
-
-    sx2 = df*dtgx;
-    sy2 = df*dtgy;
-    sz2 = df*dtgz;
-
-    f1[0] = df*dtfx;
-    f1[1] = df*dtfy;
-    f1[2] = df*dtfz;
-
-    f2[0] = sx2 - f1[0] - vb2xm*rginv*dkMult*p;
-    f2[1] = sy2 - f1[1] - vb2ym*rginv*dkMult*p;
-    f2[2] = sz2 - f1[2] - vb2zm*rginv*dkMult*p;
-
-    f4[0] = df*dthx;
-    f4[1] = df*dthy;
-    f4[2] = df*dthz;
-
-    f3[0] = -sx2 - f4[0] + vb2xm*rginv*dkMult*p;
-    f3[1] = -sy2 - f4[1] + vb2ym*rginv*dkMult*p;
-    f3[2] = -sz2 - f4[2] + vb2zm*rginv*dkMult*p;
-
-    // apply force to each of 4 atoms
-
-    if (newton_bond || i1 < nlocal) {
-      f[i1][0] += f1[0];
-      f[i1][1] += f1[1];
-      f[i1][2] += f1[2];
-    }
-
-    if (newton_bond || i2 < nlocal) {
-      f[i2][0] += f2[0];
-      f[i2][1] += f2[1];
-      f[i2][2] += f2[2];
-    }
-
-    if (newton_bond || i3 < nlocal) {
-      f[i3][0] += f3[0];
-      f[i3][1] += f3[1];
-      f[i3][2] += f3[2];
-    }
-
-    if (newton_bond || i4 < nlocal) {
-      f[i4][0] += f4[0];
-      f[i4][1] += f4[1];
-      f[i4][2] += f4[2];
-    }
-
-    if (evflag)
-      ev_tally(i1,i2,i3,i4,nlocal,newton_bond,edihedral,f1,f3,f4,
-               vb1x,vb1y,vb1z,vb2x,vb2y,vb2z,vb3x,vb3y,vb3z);
-  }
-}
-
-/* ---------------------------------------------------------------------- */
-
-void DihedralBreakable::allocate()
-{
-  allocated = 1;
-  int n = atom->ndihedraltypes;
-
-  memory->create(k,n+1,"dihedral:k");
-  memory->create(cos_shift,n+1,"dihedral:cos_shift");
-  memory->create(sin_shift,n+1,"dihedral:sin_shift");
-
-  memory->create(setflag,n+1,"dihedral:setflag");
-  for (int i = 1; i <= n; i++) setflag[i] = 0;
-
-  //new variables for breakable bond
-  memory->create(shift,n+1,"dihedral:shift");
-  memory->create(rEq,n+1,"dihedral:rEq");
-  memory->create(rCut,n+1,"dihedral:rCut");
-  memory->create(vShift,n+1,"dihedral:vShift");
-}
-
-/* ----------------------------------------------------------------------
-   set coeffs for one type
-------------------------------------------------------------------------- */
-
-void DihedralBreakable::coeff(int narg, char **arg)
-{
-  if (narg != 6) error->all(FLERR,"Incorrect args for dihedral coefficients");
-  if (!allocated) allocate();
-
-  int ilo,ihi;
-  force->bounds(arg[0],atom->ndihedraltypes,ilo,ihi);
-
-  double k_one = force->numeric(FLERR,arg[1]);
-  
-  //original comments : 
-  // arbitrary phase angle shift could be allowed, but would break
-  // backwards compatibility and is probably not needed
-  
-  // arbitrary shift is introduced
-  double shift_one = force->numeric(FLERR,arg[2]);
-
-  double rEq_one = force->numeric(FLERR,arg[3]);
-  double rCut_one = force->numeric(FLERR,arg[4]);
-  
-  if (rEq_one >= rCut_one) error->all(FLERR,"rCut must be greater than rEq");
-
-  double vShift_one = force->numeric(FLERR,arg[5]); 
-
-  int count = 0;
-  for (int i = ilo; i <= ihi; i++) {
-    k[i] = k_one;
-    
-    cos_shift[i] = cos(MY_PI*shift_one/180.0);
-    sin_shift[i] = sin(MY_PI*shift_one/180.0); 
-    
-    setflag[i] = 1;
-
-    shift[i] = shift_one;
-    rEq[i] = rEq_one;
-    rCut[i] = rCut_one;
-    vShift[i] = vShift_one;
-
-    count++;
-  }
-
-  if (count == 0) error->all(FLERR,"Incorrect args for dihedral coefficients");
-}
-
-/* ----------------------------------------------------------------------
-   proc 0 writes out coeffs to restart file
-------------------------------------------------------------------------- */
-
-void DihedralBreakable::write_restart(FILE *fp)
-{
-  fwrite(&k[1],sizeof(double),atom->ndihedraltypes,fp);
-  fwrite(&shift[1],sizeof(double),atom->ndihedraltypes,fp);
-  fwrite(&rEq[1],sizeof(double),atom->ndihedraltypes,fp);
-  fwrite(&rCut[1],sizeof(double),atom->ndihedraltypes,fp);
-  fwrite(&vShift[1],sizeof(double),atom->ndihedraltypes,fp);
-}
-
-/* ----------------------------------------------------------------------
-   proc 0 reads coeffs from restart file, bcasts them
-------------------------------------------------------------------------- */
-
-void DihedralBreakable::read_restart(FILE *fp)
-{
-  allocate();
-
-  if (comm->me == 0) {
-    fread(&k[1],sizeof(double),atom->ndihedraltypes,fp);
-    fread(&shift[1],sizeof(double),atom->ndihedraltypes,fp);
-    fread(&rEq[1],sizeof(double),atom->ndihedraltypes,fp);
-    fread(&rCut[1],sizeof(double),atom->ndihedraltypes,fp);
-    fread(&vShift[1],sizeof(double),atom->ndihedraltypes,fp);
-  }
-  MPI_Bcast(&k[1],atom->ndihedraltypes,MPI_DOUBLE,0,world);
-  MPI_Bcast(&shift[1],atom->ndihedraltypes,MPI_DOUBLE,0,world);
-  MPI_Bcast(&rEq[1],atom->ndihedraltypes,MPI_DOUBLE,0,world);
-  MPI_Bcast(&rCut[1],atom->ndihedraltypes,MPI_DOUBLE,0,world);
-  MPI_Bcast(&vShift[1],atom->ndihedraltypes,MPI_DOUBLE,0,world);
-
-  for (int i = 1; i <= atom->ndihedraltypes; i++) {
-    setflag[i] = 1;
-      
-    cos_shift[i] = cos(MY_PI*shift[i]/180.0);
-    sin_shift[i] = sin(MY_PI*shift[i]/180.0); 
-    
-  }
-}
-
-/* ----------------------------------------------------------------------
-   proc 0 writes to data file
-------------------------------------------------------------------------- */
-
-void DihedralBreakable::write_data(FILE *fp)
-{
-  for (int i = 1; i <= atom->ndihedraltypes; i++)
-    fprintf(fp,"%d %g %g %g %g %g\n",i,k[i],shift[i],rEq[i],rCut[i],vShift[i]);
-}
-
+*/
